35cd5e7f147e0990eb6310411d9be7e6b5896c56
Ryan Dy <ryan.dy@gmail.com>
2010-11-25 10:28:53 -0800

    merge sass_option instead of hard coding them

--- app/controllers/sass_on_heroku_with_rack/stylesheets_controller.rb
+++ app/controllers/sass_on_heroku_with_rack/stylesheets_controller.rb
@@ -4,9 +4,10 @@ class SassOnHerokuWithRack::StylesheetsController < ActionController::Metal
   def show
     response.headers['Cache-Control'] = "public, max-age=#{1.year.seconds.to_i}" if Rails.env.production?
 
-    sass_options = {:syntax => :scss, :load_paths => ['app/stylesheets']}
-    sass_options.merge!(:style => :compressed) unless Rails.env.development?
-    filename = Rails.root.join('app', 'stylesheets', "#{params[:stylesheet]}.scss")
-    render :text => Sass::Engine.new(File.open(filename, 'r').read, sass_options).render, :content_type => 'text/css'
+    sass_options = Sass::Plugin.options
+    sass_options.merge! :style => :compressed unless Rails.env.development?
+    extension = sass_options[:syntax] == :scss ? 'scss' : 'sass'
+    filename = Rails.root.join('app', 'stylesheets', "#{params[:stylesheet]}.#{extension}")
+    render :text => Sass::Engine.new(File.read(filename), sass_options).render, :content_type => 'text/css'
   end
 end
